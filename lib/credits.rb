f0ff84912ab5c628714655c50d35e71240120e37
Ekultek <thomas.perkins23@icloud.com>
2016-04-09 01:16:23 -0600

    Added credits to contributors 1.0.3 release

--- lib/credits.rb
+++ lib/credits.rb
@@ -1,9 +1,9 @@
 module Credits
   def credits
     print 'Credits to my creator: '.white, "Ekultek, he's a beast!\n".blue.bold,
+          'Credits to the ones that helped create me: '.white, "xuchunyang\n".blue.bold,
           'I am currently version: '.white, "#{VERSION}, stick around for upgrades!\n".blue.bold,
           'My name is: '.white, "Whitewidow, that's right albino widow\n".blue.bold,
-         puts
          puts
   end
 end
