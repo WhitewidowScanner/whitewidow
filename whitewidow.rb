7c78ec9d36d58c45fb7edaf8503c985c61ed4009
Ekultek <thomas.perkins23@icloud.com>
2016-05-03 08:10:46 -0500

    Pre-release v 1.0.7, download zip or tar file, this will not run

--- whitewidow.rb
+++ whitewidow.rb
@@ -1,79 +1,5 @@
 #!/usr/local/env ruby
 
-########################################################################################################################
-#
-# Welcome to the whitewidow source code, below you will find a whole ton of rescue clauses, yes they are necessary.
-# Do to the amount of possible errors it's very hard to catch them all, I'm still finding new errors from RestClient,
-# Nokogiri, Kernel, etc. I did a count of how many I've found so far and it's added up to about 60. Now having said
-# that, there are defiantly a couple known bugs that are being worked on.
-#
-########################################################################################################################
-#
-# Known Bugs:
-#
-# - One known bug is that every now and then there's an encoding error, I haven't really figured out how to fix this bug
-#   yet, but I am working on it. This bug, however, won't error out the program and will continue running, it looks
-#   like something along the lines of: 'IO encoder error <some hex> <some more hex>'
-#   If you have any information on how to fix this error please let me know. Make a bug report or something, fork the
-#   project and start working on it. Do whatever you need to do, to get that thing to out of the way.
-#
-# - Another known bug has something to do with how the search queries react when the program is run in default mode.
-#   Occasionally you will get a search that looks something like this: searches/preferencess?en=la
-#   When you receive this search you may ignore it, because the program will take it as a non exploitable, however,
-#   I would like to ask for your help in this matter, when you receive a search result that looks like that one
-#   (THE END (en=la) WILL BE DIFFERENT FOR YOU!!) Please scroll down to where you get the urls and add your search result to
-#   where it says #<= ADD HERE =>
-#   This will require you to fork the repo, and contribute, you may also list the search result in the bug reports
-#   HERE: https://github.com/Ekultek/whitewidow/issues/8
-#
-########################################################################################################################
-#
-# TODO list:
-#
-# - Add advanced decoding, what advanced decoding will do is attempt to decode multiple parameters of a URL. So if a URL
-#   has more than one equal sign if the first attempts fail, it will attempt to inject SQL vuln syntax into the other
-#   equal sign parameters
-#
-# - Fix IO encoder error, I have a general idea on how to fix this error, the only problem is it keeps happening when
-#   I'm not ready for it to (shocking right?). So I'm working on this, don't worry. I'm about 90% sure that it has
-#   something to do with the fact that the URL is multi encoded, so I'll have to figure out a way to decode the URL's
-#   further if the program throws an encoding error.
-#
-# - Make whitewidow capable of scraping more then one page. Right now whitewidow can only scrape a max of 10 URL's at
-#   a time. That's the first page of Google if you're lucky. However the mechanize gem does indeed give the ability
-#   to sort through pages, no I am not good with the mechanize gem. I am still trying to figure out how to use it.
-#
-# - Will be adding a feature where the program will automatically delete bad search queries, and replace them with
-#   possible good search queries. For example if a search query produces no results it will be deleted, and replaced by
-#   another one that will be in a separate file. This will be added along with the upgrade of scraping multiple pages.
-#   I have begun working on this, the search query list that will replace the deleted queries will be encoded, and I will
-#   be adding a module to decode and add the search queries into the file they will be deleted from
-#
-########################################################################################################################
-#
-# Some basic information:
-#
-# So since you're here, and reading this, I'm guessing you want to learn how to use this program, well it's pretty
-# simple, as of the new release whitewidow no longer uses ARGV, and uses optparser to parse your options, read the
-# readme because your options have changed!
-#
-# - To scan a file containing the URL's:
-#
-#   ruby whitewidow.rb -f <path/to/file>
-#
-#   File will automatically be formatted, so if there are spaces or problems with the file, it will be formatted and
-#   run as #sites.txt, your original file will also remain so don't worry about that.
-#
-# - To run whitewidow in default mode, and scrape Google for URL's:
-#
-#   ruby whitewidow.rb -d
-#
-#   This will scrape Google for the URL's by using one of the search queries in the lib directory. There's a bunch of
-#   them so chances are you won't get the same one twice in a row. However, there are no guarantees that you will
-#   find vulnerable sites this way. The easiest way is to pull a bunch of URL's and run them through this program.
-#
-########################################################################################################################
-
 require 'rubygems'
 require 'bundler/setup'
 require 'mechanize'
@@ -89,12 +15,16 @@ require 'optparse'
 require 'tempfile'
 require 'socket'
 require 'net/http'
-require_relative 'lib/modules/format.rb'
+require 'test/unit'
+
 require_relative 'lib/modules/credits.rb'
 require_relative 'lib/modules/legal.rb'
 require_relative 'lib/modules/spider.rb'
-require_relative 'lib/modules/copy.rb'
 require_relative 'lib/modules/site_info.rb'
+require_relative 'lib/modules/tools/blackwidow.rb'
+require_relative 'lib/modules/tools/format.rb'
+require_relative 'lib/modules/tools/copy.rb'
+require_relative 'lib/modules/tools/examples/run_examples.rb'
 
 include Format
 include Credits
@@ -102,6 +32,8 @@ include Legal
 include Whitewidow
 include Copy
 include SiteInfo
+include BlackWidow
+include TestExamples
 
 PATH = Dir.pwd
 VERSION = Whitewidow.version
@@ -116,17 +48,21 @@ def usage_page
 end
 
 def examples_page
-  Format.usage('This is my examples page, I\'ll show you a few examples of how to get me to do what you want.')
-  Format.usage('Running me with a file: whitewidow.rb -f <path/to/file> keep the file inside of one of my directories.')
-  Format.usage('Running me default, if you don\'t want to use a file, because you don\'t think I can handle it, or for whatever reason, you can run me default by passing the Default flag: whitewidow.rb -d this will allow me to scrape Google for some SQL vuln sites, no guarentees though!')
-  Format.usage('Running me with my Help flag will show you all options an explanation of what they do and how to use them')
-  Format.usage('Running me without a flag will show you the usage page. Not descriptive at all but gets the point across')
+  Format.prompt("Would you like to run a full test: ")
+  if $stdin.start_with?('YE')
+    #TODO Run test modules and classes from here
+  else
+    #TODO Do something else idk what you're going to do but do it here.
+  end
 end
 
 OptionParser.new do |opt|
-  opt.on('-f FILE', '--file FILE', 'Pass a file name to me, remember to drop the first slash. /tmp/txt.txt <= INCORRECT tmp/text.txt <= CORRECT') { |o| OPTIONS[:file] = o }
+  opt.on('-f=FILE', '--file=FILE', 'Pass a file name to me, remember to drop the first slash. /tmp/txt.txt <= INCORRECT tmp/text.txt <= CORRECT') { |o| OPTIONS[:file] = o }
   opt.on('-d', '--default', 'Run me in default mode, this will allow me to scrape Google using my built in search queries.') { |o| OPTIONS[:default] = o }
-  opt.on('-e', '--example', 'Shows my example page, gives you some pointers on how this works.') { |o| OPTIONS[:example] = o }
+  opt.on('-e=INPUT', '--example=INPUT', 'Shows my example page, gives you some pointers on how this works.') { |o| OPTIONS[:example] = o }
+  opt.on('--tor', 'Run through thr Tor tunnel on localhost') { |o| OPTIONS[:tor] = o }
+  opt.on('-p=PROXY', '--proxy=PROXY', 'Use your own personal proxy instead of running through Tor tunnel') { |o| OPTIONS[:proxy] = o }
+  opt.on('--thread=INTEGER', 'Require this many threads to be run default is 1') { |o| OPTIONS[:thread] = o }
 end.parse!
 
 def page(site)
@@ -170,7 +106,7 @@ def get_urls
   Format.info("I'm searching for possible SQL vulnerable sites, using search query #{SEARCH}")
   agent = Mechanize.new
   agent.user_agent = @user_agent
-  page = agent.get('http://www.google.com/')
+  page = agent.get(File.readlines("#{PATH}/lib/lists/search_engines.txt").to_s)
   google_form = page.form('f')
   google_form.q = "#{SEARCH}"
   url = agent.submit(google_form, google_form.buttons.first)
@@ -179,8 +115,7 @@ def get_urls
       str = link.href.to_s
       str_list = str.split(%r{=|&})
       urls = str_list[1]
-      next if urls.split('/')[2].start_with? 'stackoverflow.com', 'github.com', 'www.sa-k.net', 'yoursearch.me', 'search1.speedbit.com', 'duckfm.net', 'search.clearch.org', 'webcache.googleusercontent.com'
-      next if urls.start_with?('settings/ads/preferences?hl=en') #<= ADD HERE REMEMBER A COMMA =>
+      next if urls.split('/')[2].start_with?(File.readlines("#{PATH}/lib/lists/skip/blacklist.txt"))
       urls_to_log = URI.decode(urls)
       Format.success("Site found: #{urls_to_log}")
       sleep(1)
