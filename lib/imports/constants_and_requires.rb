1586db69e24c2272b9a9d9822357e45ffaa4e6eb
Ekultek <thomas.perkins23@icloud.com>
2016-08-23 12:11:18 -0500

    Redid user agent yaml, works now

--- lib/imports/constants_and_requires.rb
+++ lib/imports/constants_and_requires.rb
@@ -28,22 +28,23 @@ include Copy
 include SiteInfo
 
 # Constants used throughout the program
-USER_AGENTS = { # Temporary fix for user agents until I can refactor the YAML file
-    1 => 'Mozilla/5.0 (compatible; 008/0.83; http://www.80legs.com/webcrawler.html) Gecko/2008032620',
-    2 => 'Mozilla/5.0 (compatible; U; ABrowse 0.6; Syllable) AppleWebKit/420+ (KHTML, like Gecko)',
-    3 => 'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.2.3pre) Gecko/20100403 Lorentz/3.6.3plugin2pre (.NET CLR 4.0.20506)',
-    4 => 'Mozilla/5.0 (compatible; Yahoo! Slurp; http://help.yahoo.com/help/us/ysearch/slurp)',
-    5 => 'igdeSpyder (compatible; igde.ru; +http://igde.ru/doc/tech.html)',
-    6 => 'larbin_2.6.3 (ltaa_web_crawler@groupes.epfl.ch)',
-    7 => 'Mozilla/5.0 (Linux; Android 5.0.2; SAMSUNG SM-T550 Build/LRX22G) AppleWebKit/537.36 (KHTML, like Gecko) SamsungBrowser/3.3 Chrome/38.0.2125.102 Safari/537.36',
-    8 => 'Dalvik/2.1.0 (Linux; U; Android 6.0.1; Nexus Player Build/MMB29T)',
-    9 => 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:15.0) Gecko/20100101 Firefox/15.0.1',
-    10 => 'Mozilla/5.0 (compatible; bingbot/2.0; +http://www.bing.com/bingbot.htm)',
-}
+#USER_AGENTS = { # Temporary fix for user agents until I can refactor the YAML file
+#    1 => 'Mozilla/5.0 (compatible; 008/0.83; http://www.80legs.com/webcrawler.html) Gecko/2008032620',
+#    2 => 'Mozilla/5.0 (compatible; U; ABrowse 0.6; Syllable) AppleWebKit/420+ (KHTML, like Gecko)',
+#    3 => 'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.2.3pre) Gecko/20100403 Lorentz/3.6.3plugin2pre (.NET CLR 4.0.20506)',
+#    4 => 'Mozilla/5.0 (compatible; Yahoo! Slurp; http://help.yahoo.com/help/us/ysearch/slurp)',
+#    5 => 'igdeSpyder (compatible; igde.ru; +http://igde.ru/doc/tech.html)',
+#    6 => 'larbin_2.6.3 (ltaa_web_crawler@groupes.epfl.ch)',
+#    7 => 'Mozilla/5.0 (Linux; Android 5.0.2; SAMSUNG SM-T550 Build/LRX22G) AppleWebKit/537.36 (KHTML, like Gecko) SamsungBrowser/3.3 Chrome/38.0.2125.102 Safari/537.36',
+#    8 => 'Dalvik/2.1.0 (Linux; U; Android 6.0.1; Nexus Player Build/MMB29T)',
+#    9 => 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:15.0) Gecko/20100101 Firefox/15.0.1',
+#    10 => 'Mozilla/5.0 (compatible; bingbot/2.0; +http://www.bing.com/bingbot.htm)',
+#}
+agents = YAML.load_file('lib/lists/rand_agents.yml')
 PATH = Dir.pwd
 INFO = Whitewidow::Credits.new
 SEARCH = File.readlines("#{PATH}/lib/lists/search_query.txt").sample
 OPTIONS = {}
 URLS = /https?:\/\/[\S]+/
 COPY = Copy::CopyToFile.new
-@user_agent = USER_AGENTS[rand(1..10)]
+@user_agent = agents['user_agents'][rand(1..39)]
