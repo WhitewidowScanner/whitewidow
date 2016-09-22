0175decb29b6388ec3a49b7bace40b81681181af
Ekultek <thomas.perkins23@icloud.com>
2016-09-21 20:50:27 -0500

    Removed unused code

--- lib/modules/expansion/string_expan.rb
+++ lib/modules/expansion/string_expan.rb
@@ -2,25 +2,13 @@
 # String expansion for colored output
 #
 class String
-  def black;          "\e[30m#{self}\e[0m" end
   def red;            "\e[31m#{self}\e[0m" end
   def green;          "\e[32m#{self}\e[0m" end
-  def brown;          "\e[33m#{self}\e[0m" end
   def blue;           "\e[34m#{self}\e[0m" end
   def purple;         "\e[35m#{self}\e[0m" end
   def cyan;           "\e[36m#{self}\e[0m" end
-  def gray;           "\e[37m#{self}\e[0m" end
   def yellow;         "\e[33m#{self}\e[0m" end
   def white;          "\e[37m#{self}\e[0m" end
-
-  def bg_black;       "\e[40m#{self}\e[0m" end
-  def bg_red;         "\e[41m#{self}\e[0m" end
-  def bg_green;       "\e[42m#{self}\e[0m" end
-  def bg_brown;       "\e[43m#{self}\e[0m" end
-  def bg_blue;        "\e[44m#{self}\e[0m" end
-  def bg_magenta;     "\e[45m#{self}\e[0m" end
-  def bg_cyan;        "\e[46m#{self}\e[0m" end
-  def bg_gray;        "\e[47m#{self}\e[0m" end
 
   def bold;           "\e[1m#{self}\e[0m" end
 end
