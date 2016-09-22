2fec270e3b84ba32d023e1ce46841c579367462d
Ekultek <thomas.perkins23@icloud.com>
2016-09-21 20:53:22 -0500

    Minor fixes

--- lib/modules/copy.rb
+++ lib/modules/copy.rb
@@ -1,11 +1,11 @@
 module Copy
 
   # Copy from file to file instead of using the IO steam method.
-  # @param [String] file1
-  # @param [String] file2
-  def file(file1, file2)
-    File.open(file1, "a+").each_line do |s|
-      File.open(file2, "a+") { |vul| vul.puts(s) }
+  # @param [String] from_file
+  # @param [String] to_file
+  def file(from_file, to_file)
+    File.open(from_file, "a+").each_line do |copy|
+      File.open(to_file, "a+") { |vul| vul.puts(copy) }
     end
   end
 
