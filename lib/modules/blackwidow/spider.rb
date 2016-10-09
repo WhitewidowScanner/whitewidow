242396f3fa86ce42576c9374d5a476fd4b2d02fc
Ekultek <thomas.perkins23@icloud.com>
2016-10-09 13:56:53 -0500

    working on a recursive spider bot

---
+++ lib/modules/blackwidow/spider.rb
@@ -0,0 +1,30 @@
+require 'mechanize'
+require 'open-uri'
+require 'uri'
+
+module BlackWidow
+
+  class RecursiveSpider
+
+    def initialize(site)
+      @site = site
+    end
+
+    def pull_urls
+      URI.extract(open(@site).read).grep(/\w+:\/\/[\w.-]+(?::?\d{1,5})?[-\w.\/?=&%]*/).each do |link|
+        File.open("blackwidow_links.txt", "a+") { |url| url.puts(link) }
+      end
+    end
+
+    def follow_links
+      File.open("blackwidow_links.txt", "r").each_line do |link|
+        p URI.extract(open(link).read).grep(/\w+:\/\/[\w.-]+(?::?\d{1,5})?[-\w.\/?=&%]*/)
+      end
+    end
+
+  end
+
+end
+
+test = BlackWidow::RecursiveSpider.new("http://www.distance-cities.com/distance-fayetteville-ar-to-prairie-grove-ar")
+test.follow_links
