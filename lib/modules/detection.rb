efb1a48aa0ca1ce40519e3b8997424897ebab668
Ekultek <thomas.perkins23@icloud.com>
2016-10-09 14:07:19 -0500

    fixed comments

--- lib/modules/detection.rb
+++ lib/modules/detection.rb
@@ -6,8 +6,8 @@ module MultipleParameters
   class TestAllParameters
 
     # Add if multiple parameters are found
-    #
     # @param [String] site to check for
+    # @param [Syntax] SQL syntax to inject
     def check_for_multiple_parameters(site, syntax)
       if site.scan("=").count != 1
         site.sub!("=", "=" + syntax)
