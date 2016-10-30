12a6eb4c44a89886910a116f27c94eba090d34e2
Ekultek <thomas.perkins23@icloud.com>
2016-10-30 14:52:12 -0500

    edited for new release

--- lib/modules/blackwidow.rb
+++ lib/modules/blackwidow.rb
@@ -4,24 +4,20 @@ module BlackWidow
 
   class RecursiveSpider
 
-    def initialize(url)
-      @url = url
-    end
-
-    def pull_links
+    def pull_links(url)
       # Extract all the urls and throw them into an array
-      data = parse(@url, 'html', 0)
+      data = parse(url, 'html', 0)
       return URI.extract(data, ['http', 'https'])
     end
 
-    def follow_links(urls)
+    def follow_links(url_arr)
       # Do the same thing with the url array
-      urls.each do |link|
+      url_arr.each do |link|
         search = parse(link, 'html', 0)
         data = URI.extract(search, %w(http https))
         data.each do |url|
           FORMAT.info("Found #{url}.")
-          File.open("#{PATH}/log/blackwidow_log.txt", "a+") { |to_search| to_search.write(url) }
+          File.open("#{PATH}/tmp/blackwidow_log.txt", "a+") { |to_search| to_search.write(url + "\n") }
           sleep(1)
         end
       end
