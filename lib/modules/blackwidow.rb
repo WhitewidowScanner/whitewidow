667fd86ab19fdc052d3b2b845d4cc418ada3faec
Ekultek <thomas.perkins23@icloud.com>
2016-11-03 18:13:21 -0500

    Minor text fixes

--- lib/modules/blackwidow.rb
+++ lib/modules/blackwidow.rb
@@ -1,17 +1,22 @@
+#
 # Recursive spider search, runs through all the urls and grabs that sites urls
-
+#
 module BlackWidow
 
   class RecursiveSpider
 
+    #
+    # Extracts all the links from a url
+    #
     def pull_links(url)
-      # Extract all the urls and throw them into an array
       data = parse(url, 'html', 0)
       return URI.extract(data, ['http', 'https'])
     end
 
+    #
+    # Extract the links from that URL as well and save them into a file
+    #
     def follow_links(url_arr)
-      # Do the same thing with the url array
       url_arr.each do |link|
         search = parse(link, 'html', 0)
         data = URI.extract(search, %w(http https))
