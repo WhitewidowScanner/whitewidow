c67984a2c733acb05ea63461d9584e78f669393c
Ekultek <thomas.perkins23@icloud.com>
2016-11-03 18:14:31 -0500

    More comment fixes

--- lib/modules/expansion/string_expan.rb
+++ lib/modules/expansion/string_expan.rb
@@ -2,6 +2,7 @@
 # String expansion for colored output
 #
 class String
+
   def red;            "\e[31m#{self}\e[0m" end
   def green;          "\e[32m#{self}\e[0m" end
   def blue;           "\e[34m#{self}\e[0m" end
@@ -11,4 +12,5 @@ class String
   def white;          "\e[37m#{self}\e[0m" end
 
   def bold;           "\e[1m#{self}\e[0m" end
+
 end
