dcbf2c7ba8be9c439e3469e51e3e3200177b7cd6
Ekultek <thomas.perkins23@icloud.com>
2016-11-03 18:13:57 -0500

    Comment fixes

--- lib/modules/detection.rb
+++ lib/modules/detection.rb
@@ -5,12 +5,12 @@ module MultipleParameters
 
   class TestAllParameters
 
-    # Add if multiple parameters are found
-    # @param [String] site to check for
-    # @param [Syntax] SQL syntax to inject
+    #
+    # Add injection syntax for every "=" in the URL
+    #
     def check_for_multiple_parameters(site, syntax)
       if site.scan("=").count != 1
-        site.sub!("=", "=" + syntax)
+        site.sub!("=", "=" + syntax)  # TODO: Add the syntax one at at a time and remove the last one added
       end
     end
 
