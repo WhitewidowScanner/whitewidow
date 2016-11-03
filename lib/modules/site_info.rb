b0e9235be30a51b4f87ede290f06b514279c30e4
Ekultek <thomas.perkins23@icloud.com>
2016-11-03 18:15:07 -0500

    And guess what? MORE COMMENTSgit add lib

--- lib/modules/site_info.rb
+++ lib/modules/site_info.rb
@@ -1,20 +1,25 @@
-# Capture information on the site that is available through the SQL
+#
+# Pull the information of the site that is available. Pulls the server (apache, redhat, etc) and the IP address
+# that the site resolves to
+#
 module SiteInfo
 
-  # @param [String] site
+  #
   # Get the IP address that the site resolves to using Socket
+  #
   def self.capture_ip(site)
     uri = URI.parse(site)
     true_url = uri.hostname
     return IPSocket::getaddress(true_url)
   end
 
-  # @param [String] site
+  #
   # Get the site host name using Net::HTTP
+  #
   def self.capture_host(site)
     uri = URI.parse(site)
     res = Net::HTTP.get_response(uri)
-    return res['server'] # Pull it from the hash
+    return res['server'] # Pull it from the hash that is created
   end
 
 end
