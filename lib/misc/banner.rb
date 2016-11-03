057dd58291a0977dd8599203a34e9acf22546c3a
Ekultek <thomas.perkins23@icloud.com>
2016-11-03 18:11:48 -0500

    Bumped version number

--- lib/misc/banner.rb
+++ lib/misc/banner.rb
@@ -17,7 +17,7 @@ module Whitewidow
 
     # The version the program is currently in
     def version
-      '1.5.0' # Version number
+      '1.5.0.1' # Version number <major>.<minor>.<patch>.<monthly commit>
     end
 
   end
