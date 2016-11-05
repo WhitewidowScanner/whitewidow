62f7db8353ab5e2e133d4668902d86b4a5063b04
Ekultek <thomas.perkins23@icloud.com>
2016-11-05 14:59:28 -0500

    edited the version type

--- lib/misc/banner.rb
+++ lib/misc/banner.rb
@@ -17,7 +17,32 @@ module Whitewidow
 
     # The version the program is currently in
     def version
-      '1.5.1.2' # Version number <major>.<minor>.<patch>.<monthly commit>
+      '1.5.1.4' # Version number <major>.<minor>.<patch>.<monthly commit>
+    end
+
+    def page(site)
+      Nokogiri::HTML(RestClient.get(site))
+    end
+    # Need these to get it to output the version number correctly
+    def parse(site, tag, i)
+      parsing = page(site)
+      parsing.css(tag)[i].to_s
+    end
+
+    #
+    # The type of version you have, usually it'll be stable or dev
+    #
+    def version_type(version)
+      data = parse("https://github.com/Ekultek/whitewidow/blob/master/lib/misc/banner.rb", "td", 39)
+      arr = data.split(" ")
+      version_number = arr[7][/(\d+\.)?(\d+\.)?(\d+\.)?(\*|\d+)/]
+      if version_number.count(".") > 2
+        return "dev".yellow.bold
+      elsif version_number != version
+        return "Upgrade Available".red.bold
+      else
+        return "stable".green.bold
+      end
     end
 
   end
