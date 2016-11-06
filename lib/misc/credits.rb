fb14dcfd9d69ea6f54aba818e0eb39101d253013
Ekultek <thomas.perkins23@icloud.com>
2016-11-06 13:50:08 -0600

    edited for new version, has pretty pictures

--- lib/misc/credits.rb
+++ lib/misc/credits.rb
@@ -4,7 +4,7 @@ module Credits
   # Outputs the version number and credits to myself because I'm an arrogant fuck head.
   def credits
     puts 'Program created and written by: '.blue + "Ekultek".green.bold
-    puts 'Current version: '.blue + VERSION + '(' + VERSION_TYPE + ')'
+    puts 'Current version: '.blue + VERSION.green.bold + '(' + VERSION_TYPE + ')'
   end
 
 end
