375ce0c45854848424a229f37048b8af8fb9a001
Ekultek <thomas.perkins23@icloud.com>
2016-11-06 13:53:30 -0600

    deprecated kept for backup

--- lib/misc/credits.rb
+++ lib/misc/credits.rb
@@ -1,6 +1,8 @@
 # Credits of the program
 module Credits
 
+  # Deprecated since version 1.6.0
+
   # Outputs the version number and credits to myself because I'm an arrogant fuck head.
   def credits
     puts 'Program created and written by: '.blue + "Ekultek".green.bold
