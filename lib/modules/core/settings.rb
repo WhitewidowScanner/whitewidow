0f6f4e0957265c33c4fcb88b83cc352b7d96c3ec
Ekultek <thomas.perkins23@icloud.com>
2016-11-19 13:38:06 -0600

    edited the settings for default sqlmap configuration

--- lib/modules/core/settings.rb
+++ lib/modules/core/settings.rb
@@ -10,14 +10,26 @@ module Settings
       Nokogiri::HTML(RestClient::Request.execute(:url => site, :method => :get, :verify_ssl => false)) # Fix #26 https://github.com/Ekultek/whitewidow/issues/26
     end
 
-#
-# This is actually pretty smart, it's used to parse the HTML
-# @param [String] site url
-# @param [String] tag css selector
-# @param [Integer] i the given html row
+    #
+    # This is actually pretty smart, it's used to parse the HTML
+    # @param [String] site url
+    # @param [String] tag css selector
+    # @param [Integer] i the given html row
     def parse(site, tag, i)
       parsing = page(site)
       parsing.css(tag)[i].to_s
+    end
+
+    def sqlmap_config
+      data = File.open("#{PATH}/lib/lists/default_sqlmap_config.txt", "a+")
+      if data.read == "false"
+        commands = FORMAT.prompt("Enter sqlmap commands, bulkfile is already default")
+        answer = FORMAT.prompt("Would you like to make these commands your default?[y/N]")
+        answer.downcase.start_with?("y") ? File.open("#{PATH}/lib/lists/default_sqlmap_config.txt", "a+") { |config| config.write("#{commands}") } : File.open("#{PATH}/lib/lists/default_sqlmap_config.txt", "a+") { |config| config.write("false") }
+      else
+        config = File.read("#{PATH}/lib/lists/default_sqlmap_config.txt").strip!
+        return "#{config}"
+      end
     end
 
   end
