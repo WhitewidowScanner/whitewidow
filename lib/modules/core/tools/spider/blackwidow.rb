fcba96c0a7de9a7d43336bc45d740f60983b52b5
Ekultek <thomas.perkins23@icloud.com>
2016-11-20 13:36:41 -0600

    edited, it runs again successfully

--- lib/modules/core/tools/spider/blackwidow.rb
+++ lib/modules/core/tools/spider/blackwidow.rb
@@ -1,3 +1,7 @@
+require_relative '../../../../../lib/modules/core/settings'
+
+include Settings
+
 #
 # Recursive spider search, runs through all the urls and grabs that sites urls
 #
@@ -9,7 +13,7 @@ module BlackWidow
     # Extracts all the links from a url
     #
     def pull_links(url)
-      data = parse(url, 'html', 0)
+      data = Settings::ProgramSettings.new.parse(url, 'html', 0)
       return URI.extract(data, ['http', 'https'])
     end
 
@@ -18,12 +22,12 @@ module BlackWidow
     #
     def follow_links(url_arr)
       url_arr.each do |link|
-        search = parse(link, 'html', 0)
+        search = Settings::ProgramSettings.new.parse(link, 'html', 0)
         data = URI.extract(search, %w(http https))
         data.each do |url|
-          FORMAT.info("Found #{url}.")
+          FORMAT.info("Found #{url}")
           File.open("#{PATH}/tmp/blackwidow_log.txt", "a+") { |to_search| to_search.write(url + "\n") }
-          sleep(1)
+          sleep(0.2)
         end
       end
     end
