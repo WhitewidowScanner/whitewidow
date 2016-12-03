e5e6d8e34cf45215e284dc68f925a2f9acd6c375
Ekultek <thomas.perkins23@icloud.com>
2016-12-02 21:42:17 -0600

    added a regex to catch IPv6, will attempt to decode to IPv4

--- lib/modules/core/site_info.rb
+++ lib/modules/core/site_info.rb
@@ -10,7 +10,19 @@ module SiteInfo
   def self.capture_ip(site)
     uri = URI.parse(site)
     true_url = uri.hostname
-    return IPSocket::getaddress(true_url)
+    ip = IPSocket::getaddress(true_url)
+    if !(ip =~ /^(?=\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$)(?:(?:25[0-5]|[12][0-4][0-9]|1[5-9][0-9]|[1-9]?[0-9])\.?)
+                {4}$|(?=^(?:[0-9a-f]{0,4}:){2,7}[0-9a-f]{0,4}$)(?![^:]*::.+::[^:]*$)(?:(?=.*::.*)|
+                (?=\w+:\w+:\w+:\w+:\w+:\w+:\w+:\w+))(?:(?:^|:)(?:[0-9a-f]{4}|[1-9a-f][0-9a-f]{0,3})){0,8}
+                (?:::(?:[0-9a-f]{1,4}(?:$|:)){0,6})?$/)
+      return ip
+    else
+      if ip.ipv4_mapped
+        return ip.native
+      else
+        return "Unknown".red.bold
+      end
+    end
   end
 
   #
