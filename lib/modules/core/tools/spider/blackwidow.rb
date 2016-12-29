431faa915283b0ae88159fc9cd14891a4c34e129
Perkins <z-perkins-thomas@dir.labor.gov>
2016-12-29 10:39:45 -0600

    minor fix

--- lib/modules/core/tools/spider/blackwidow.rb
+++ lib/modules/core/tools/spider/blackwidow.rb
@@ -3,7 +3,7 @@ require_relative '../../../../../lib/modules/core/settings'
 include Settings
 
 #
-# Recursive spider search, runs through all the urls and grabs that sites urls
+# Recursive spider search, GRABS EVERY URL AVAILABLE
 #
 module BlackWidow
 
@@ -25,7 +25,7 @@ module BlackWidow
         search = Settings::ProgramSettings.new.parse(link, 'html', 0)
         data = URI.extract(search, %w(http https))
         data.each do |url|
-          FORMAT.info("Found #{url}")
+          FORMAT.info("Found redirect: #{url}")
           File.open("#{PATH}/tmp/blackwidow_log.txt", "a+") { |to_search| to_search.write(url + "\n") }
           sleep(0.2)
         end
