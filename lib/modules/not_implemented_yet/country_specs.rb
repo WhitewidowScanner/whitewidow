c528b5ddf4fd731921148640d6a245762bffc338
Perkins <z-perkins-thomas@dir.labor.gov>
2016-12-30 11:55:22 -0600

    playing around with it

--- lib/modules/not_implemented_yet/country_specs.rb
+++ lib/modules/not_implemented_yet/country_specs.rb
@@ -11,10 +11,12 @@ class IPFilter
     def find_country_by_ip_address(ip_address)
       ip = IPAddr.new(ip_address).to_i
       CSV.foreach('GeoIPCountry.csv') do |_, _, ip1, ip2, _, country_name|
-        return country_name if (ip1.to_i..ip2.to_i).cover?(ip)
+        return country_name if (ip1.to_i..ip2.to_i).cover?(ip)  # Return what country the IP originates from by checking the IP range
       end
     end
 
   end
 
 end
+
+puts IPFilter.find_country_by_ip_address('1.5.255.200')
